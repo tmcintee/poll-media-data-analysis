1bfaea4db966d1c8819b8f4051aaa9e54a00aaf0
DESKTOP-OJ669I3\tomas <tomas.mcintee@gmail.com>
2019-12-01 14:33:38 -0500

    Update gabbard_harris.R

--- gabbard_harris.R
+++ gabbard_harris.R
@@ -8,12 +8,46 @@ ggplot(GH_polling_averages,aes(x = Date,y = Polling, color = Candidate))+
   geom_line(size = 1)+
   #geom_smooth()+
   scale_x_date()+
+  expand_limits(y = 0)+
   #geom_vline(xintercept = parse_date("2019-08-24"))+
   #geom_rect(aes(xmin = parse_date("2019-08-24"),xmax = parse_date("2019-09-07"),ymin = 0, ymax = Inf, alpha = 0.5))+
-  geom_text(x = parse_date("2019-08-31"), y = 8, angle = 90, label = "Third debate: Gabbard attacks Harris", color = "Black", alpha = 0.5)+
+  geom_text(x = parse_date("2019-07-31"), y = 8, angle = 90, label = "Second debate: Gabbard attacks Harris", color = "Black", alpha = 0.5)+
   geom_text(x = parse_date("2019-11-20"), y = 8, angle = 90, label = "Fifth debate: Harris attacks Gabbard", color = "Black", alpha = 0.5)+
   labs(color = "Candidate",
        x = "Date",
-       y = "Two week rolling average of polls",
+       y = "Horse race polling (two week rolling weighted average)",
        title = "Harris v. Gabbard (2020)",
        text = NA)
+KH_polling <- president_primary_polls %>% filter(candidate_name == "Kamala Harris") %>% group_by(display_name) %>% summarise(mean_pct = mean(pct), count = n())
+
+ggplot(GH_polling_averages %>% filter(Date > parse_date("2019-07-31")),aes(x = Date,y = Polling, color = Candidate))+
+  geom_line(size = 1)+
+  #geom_smooth()+
+  scale_x_date()+
+  #geom_vline(xintercept = parse_date("2019-08-24"))+
+  #geom_rect(aes(xmin = parse_date("2019-08-24"),xmax = parse_date("2019-09-07"),ymin = 0, ymax = Inf, alpha = 0.5))+
+  #geom_text(x = parse_date("2019-07-31"), y = 5, angle = 90, label = "Second debate: Gabbard attacks Harris", color = "Black", alpha = 0.5)+
+  #geom_text(x = parse_date("2019-11-20"), y = 5, angle = 90, label = "Fifth debate: Harris attacks Gabbard", color = "Black", alpha = 0.5)+
+  geom_text(x = parse_date("2019-10-18"), y = 2, angle = 90, label = "Clinton attacks Gabbard", color = "Black", alpha = 0.5)+
+  labs(color = "Candidate",
+       x = "Date",
+       y = "Horse race polling (two week rolling weighted average)",
+       title = "Harris v. Gabbard (2020)",
+       text = NA)+
+  expand_limits(y = 0)
+
+ggplot(GH_polling_averages %>% filter(Date > parse_date("2019-11-19")),aes(x = Date,y = Polling, color = Candidate))+
+  geom_line(size = 1)+
+  #geom_smooth()+
+  scale_x_date()+
+  #geom_vline(xintercept = parse_date("2019-08-24"))+
+  #geom_rect(aes(xmin = parse_date("2019-08-24"),xmax = parse_date("2019-09-07"),ymin = 0, ymax = Inf, alpha = 0.5))+
+  #geom_text(x = parse_date("2019-07-31"), y = 5, angle = 90, label = "Second debate: Gabbard attacks Harris", color = "Black", alpha = 0.5)+
+  geom_text(x = parse_date("2019-11-20"), y = 5, angle = 90, label = "Fifth debate: Harris attacks Gabbard", color = "Black", alpha = 0.5)+
+  #geom_text(x = parse_date("2019-10-18"), y = 2, angle = 90, label = "Clinton attacks Gabbard", color = "Black", alpha = 0.5)+
+  labs(color = "Candidate",
+       x = "Date",
+       y = "Horse race polling (two week rolling weighted average)",
+       title = "Harris v. Gabbard (2020)",
+       text = NA)+
+  expand_limits(y = 0)
