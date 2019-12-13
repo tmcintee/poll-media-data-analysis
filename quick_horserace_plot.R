a86e95acc6ec38590a831ea2972653694423091e
DESKTOP-OJ669I3\tomas <tomas.mcintee@gmail.com>
2019-12-12 21:40:56 -0500

    Polling data update

--- quick_horserace_plot.R
+++ quick_horserace_plot.R
@@ -22,10 +22,15 @@ temp <- current_polling_averages %>%
   summarise(mean_polling = mean(Polling)) %>%
   arrange(-mean_polling)
 
+ppp_filtered <- president_primary_polls %>% filter(candidate_name %in% current_candidates)
+ppp_filtered$Candidate <- factor(ppp_filtered$candidate_name,levels = temp$Candidate)
+
 current_polling_averages$Candidate <- factor(current_polling_averages$Candidate, levels = temp$Candidate)
 ggplot(current_polling_averages %>% filter(Polling > 1),aes(x = Date, y = Polling, color = Candidate))+
   scale_x_date()+
   scale_y_log10(limits = c(1,41), breaks = c(1,2,3,4,5,10,15,20,30,40))+
   geom_line(size = 1)+
+  #geom_point(data = ppp_filtered, aes(x = as.Date(mean(c(end_date,start_date))), y = pct), alpha = 0.1)+
   scale_color_brewer(palette = "Paired")+
   labs(title = "Horse-race polling averages (weighted, 2 week window, log scale)")
+
